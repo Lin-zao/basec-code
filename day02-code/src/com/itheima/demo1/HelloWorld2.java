67e092d2dccc43f6cd60c5079c297dde0ca44e13
linzao <298184741@qq.com>
2026-09-02 21:05:56 +0800

    测试提交github

--- day02-code/src/com/itheima/demo1/HelloWorld2.java
+++ day02-code/src/com/itheima/demo1/HelloWorld2.java
@@ -3,7 +3,6 @@ package com.itheima.demo1;
 public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        System.out.println("测试提交github");
 
 
     }
