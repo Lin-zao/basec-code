6ed18e5ab00a5c401bba503a94c4db68ba7e1c69
linzao <298184741@qq.com>
2026-09-03 19:34:59 +0800

    day03-practic

---
+++ day03-code/src/practic/arithmeticoperator.java
@@ -0,0 +1,20 @@
+package practic;
+
+public class arithmeticoperator {
+    public static void main(String[] args){
+        //整数参与计算,结果只能得到整数
+        //小数参与运算,结果有可能是不精确的小数
+        System.out.println(3 + 2);
+        System.out.println(3 - 2);
+        System.out.println(3 * 2);
+        System.out.println(5 / 2);
+        System.out.println(5.0 / 2);
+        System.out.println(3 % 2);
+        System.out.println(10 % 5);
+        System.out.println(3.1 + 2.2);
+        System.out.println(3.1 - 2.2);
+
+
+
+    }
+}
---
+++ day03-code/src/text/Text1.java
@@ -0,0 +1,25 @@
+package text;
+
+import java.util.Scanner;
+
+public class Text1 {
+    public static void main(String[] args){
+        //键盘录入一个三位数,获取其中的个位.十位,百位
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个三位数:");
+        int number = sc.nextInt();
+
+        //个位
+        int ge = number % 10;
+        int shi = number / 10 % 10;
+        int bai = number /100 % 10;
+        System.out.println("个位为:");
+        System.out.println(ge);
+        System.out.println("十位为:");
+        System.out.println(shi);
+        System.out.println("百位为:");
+        System.out.println(bai);
+
+
+    }
+}
