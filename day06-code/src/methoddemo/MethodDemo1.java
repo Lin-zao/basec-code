472a408796d7f535aaefbee5311c715b72e36f3c
linzao <298184741@qq.com>
2026-09-06 17:39:33 +0800

    Saturday, 6 September 2026-practic-add-third

---
+++ day06-code/src/methoddemo/MethodDemo1.java
@@ -0,0 +1,21 @@
+package methoddemo;
+
+public class MethodDemo1 {
+    public static void main(String[] args) {
+        //掌握最简单的顶哟方法和调用的格式
+        //调用方法
+        playGame();
+        playGame();
+    }
+    //定义一个方法
+    public static void playGame(){
+        System.out.println("选人物");
+        System.out.println("准备开局");
+        System.out.println("对线");
+        System.out.println("崩盘");
+        System.out.println("骂队友");
+        System.out.println("送人头");
+        System.out.println("GG");
+
+    }
+}
---
+++ day06-code/src/methoddemo/MethodDemo2.java
@@ -0,0 +1,13 @@
+package methoddemo;
+
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        printGirl();
+        printGirl();
+
+    }
+    public static void printGirl(){
+        System.out.println("lq");
+        System.out.println(18);
+    }
+}
