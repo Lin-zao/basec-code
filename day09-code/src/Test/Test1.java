855abab831fa8eccfbd0b8acc6e492519f301bc1
你的Github用户名 <Github>
2026-09-15 21:11:46 +0800

    Sep 15, 2026-second

---
+++ day09-code/src/Test/Test1.java
@@ -0,0 +1,30 @@
+package Test;
+
+public class Test1 {
+    private String name;
+    private int blood;
+
+    public Test1() {
+    }
+
+    public Test1(String name, int blood) {
+        this.name = name;
+        this.blood = blood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBlood() {
+        return blood;
+    }
+
+    public void setBlood(int blood) {
+        this.blood = blood;
+    }
+}
