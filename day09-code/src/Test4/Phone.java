dcc4eedaac1a5c7363da6a8b53ce8f3248f4e2ef
你的Github用户名 <Github>
2026-09-16 19:58:50 +0800

    Sep 16, 2026

---
+++ day09-code/src/Test4/Phone.java
@@ -0,0 +1,40 @@
+package Test4;
+
+public class Phone {
+    private String brand;
+    private int price;
+    private String color;
+
+    public Phone() {
+    }
+
+    public Phone(String brand, int price, String color) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
---
+++ day09-code/src/Test4/PhoneTest.java
@@ -0,0 +1,30 @@
+package Test4;
+/*定义数组存储 3 部手机对象。
+手机的属性：品牌，价格，颜色。
+要求，计算出三部手机的平均价格*/
+public class PhoneTest {
+    public static void main(String[] args) {
+        //1.创建一个数组
+        Phone[] arr = new Phone[3];
+
+        //2.创建手机对象
+        Phone p1 = new Phone("小米",4999,"黑色");
+        Phone p2 = new Phone("苹果",6999,"白色");
+        Phone p3 = new Phone("三星",48999,"蓝色");
+
+        //3.把手机对象添加在数组中
+        arr[0] = p1;
+        arr[1] = p2;
+        arr[2] = p3;
+
+        //4.获取三部手机的总值
+        int num =0;
+        for (int i = 0; i < arr.length; i++) {
+            num = num +arr[i].getPrice();
+        }
+        //5.求平均值
+        double avg = num*1.0/arr.length;
+        System.out.println(avg);
+
+    }
+}
